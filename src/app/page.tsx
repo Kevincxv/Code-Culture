a34dabaa9eed3cf9206807c04a10f2612627d403
Kevin Camacho <Kevincxv@outlook.com>
2023-12-19 13:36:48 -0500

    Disregard

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 import Link from "next/link";
-import NavBar from "./components/static-page/navBar";
-import SidePanel from "./components/static-page/sidePanel";
-import Footer from "./components/static-page/footer";
+import NavBar from "./components/static/navBar";
+import SidePanel from "./components/static/sidePanel";
+import Footer from "./components/static/footer";
 import SignUp from "./components/register/signup";
 
 export default function Home() {
