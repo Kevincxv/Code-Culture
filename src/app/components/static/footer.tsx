3b4bbc6b187b8402d897bc3d0aebd749a831b2ae
Kevin Camacho <Kevincxv@outlook.com>
2023-12-19 14:39:04 -0500

    Updated future to be with any view port

--- src/app/components/static/footer.tsx
+++ src/app/components/static/footer.tsx
@@ -3,41 +3,43 @@ import Link from "next/link";
 export default function footer() {
   return (
     <>
-      <div className="font-medium">
-        <div className=" mt-20 ml-3 flex flex-row justify-start gap-2">
-          <Link href="/">
-            <p className="text-[#B7B7B7] text-xs hover:underline">About</p>
-          </Link>
-          <Link href="/">
-            <p className=" text-[#B7B7B7] text-xs hover:underline">Help Center</p>
-          </Link>
-          <Link href="/">
-            <p className=" text-[#B7B7B7] text-xs hover:underline">
-              Content Policy
-            </p>
-          </Link>
-        </div>
-        <div className="flex ml-3 gap-2">
-          <Link href="/">
-            <p className=" text-[#B7B7B7] text-xs hover:underline">
-              Code of Conduct
-            </p>
-          </Link>
-          <Link href="/">
-            <p className=" text-[#B7B7B7] text-xs hover:underline">
-              Terms of Service
-            </p>
-          </Link>
-          <Link href="/">
-            <p className=" text-[#B7B7B7] text-xs hover:underline">More</p>
-          </Link>
-        </div>
-        <div className="flex ml-3 border-t border-gray-700 w-64">
-          <Link href="/">
-            <p className=" w-72 text-[#B7B7B7] text-xs hover:underline">
-              Code Culture, Inc &#x40; 2023. All rights reserved
-            </p>
-          </Link>
+      <div className="absolute bottom-3 left-0 w-full">
+        <div className="font-medium">
+          <div className="flex justify-start ml-3 gap-2">
+            <Link href="/">
+              <p className="text-[#B7B7B7] text-xs hover:underline">About</p>
+            </Link>
+            <Link href="/">
+              <p className=" text-[#B7B7B7] text-xs hover:underline">Help Center</p>
+            </Link>
+            <Link href="/">
+              <p className=" text-[#B7B7B7] text-xs hover:underline">
+                Content Policy
+              </p>
+            </Link>
+          </div>
+          <div className="flex ml-3 gap-2">
+            <Link href="/">
+              <p className=" text-[#B7B7B7] text-xs hover:underline">
+                Code of Conduct
+              </p>
+            </Link>
+            <Link href="/">
+              <p className=" text-[#B7B7B7] text-xs hover:underline">
+                Terms of Service
+              </p>
+            </Link>
+            <Link href="/">
+              <p className=" text-[#B7B7B7] text-xs hover:underline">More</p>
+            </Link>
+          </div>
+          <div className="flex ml-3 border-t border-gray-700 w-64">
+            <Link href="/">
+              <p className=" w-72 text-[#B7B7B7] text-xs hover:underline">
+                Code Culture, Inc &#x40; 2023. All rights reserved
+              </p>
+            </Link>
+          </div>
         </div>
       </div>
     </>
