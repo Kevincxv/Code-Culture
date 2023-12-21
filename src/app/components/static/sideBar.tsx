94a24426cadcff1486b7e454434f723760fbe652
Joseph Ruiz <Ruiz-joseph@hotmail.com>
2023-12-21 15:54:28 -0500

    subtle changes

--- src/app/components/static/sideBar.tsx
+++ src/app/components/static/sideBar.tsx
@@ -28,7 +28,7 @@ export default function sideBar() {
         <ul>
           {sideBar.map((link) =>
             <li key={link.href} className="mb-1">
-              <Link href={link.href} className="flex items-center ml-6 gap-3 transition duration-100 ease-in-out hover:bg-custom-hover hover:shadow-custom-hover rounded w-[60px] md:w-80 h-16">
+              <Link href={link.href} className={`flex items-center ml-6 gap-3 transition duration-100 ease-in-out hover:bg-custom-hover hover:shadow-custom-hover rounded w-[60px] md:w-80 h-16`}>
                 <img src={link.icon} alt={`${link.label} Page`} className="ml-3 w-9" />
                 <p className=" text-white font-bold hidden sm:block transition-opacity duration-300 opacity-0 md:opacity-100">{link.label}</p>
               </Link>
@@ -36,7 +36,7 @@ export default function sideBar() {
           )}
         </ul>
       </aside>
-      <button onClick={toggleSidebar} className={`absolute ${isSidebarOpen ? 'top-[270px] left-[360px]' : 'top-[270px] left-[20px]' } bg-white rounded-full p-2 transition-width duration-300 ease-in-out`}>
+      <button onClick={toggleSidebar} className={`absolute opacity-0 md:opacity-100 ${isSidebarOpen ? 'top-[270px] left-[360px]' : 'top-[270px] left-[20px]' } bg-white rounded-full p-2 transition-width duration-300 ease-in-out`}>
       </button>
     </div>
   )
